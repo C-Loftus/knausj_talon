e3b1df46fed13f55f4bf1f5983a600e17016bd5b
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-06-13 16:43:50 -0400

    document pull

--- pull.sh
+++ pull.sh
@@ -2,3 +2,17 @@ git fetch community
 git pull
 git checkout main
 git merge community/main
+
+
+# Can you just have multiple branches in your fork to work from? One holding the stuff you want backed up, and one holding something synced with community for you to contribute from If that works for you then the command is git remote add upstream <url>. Then run git fetch upstream , create a new branch, switch to it, and run git reset --hard upstream/main . Then your branch is at base community. Switch upstream out for whatever you want to call community. 
+
+# git checkout bare_upstream
+# git branch
+# git pull upstream main
+# git pull upstream
+# git reset --hard upstream/main
+# git checkout -b "bare_upstream"
+# git fetch upstream
+# git remote -v
+# git remote add upstream https://github.com/talonhub/community
+# cd .knausj_talon/
