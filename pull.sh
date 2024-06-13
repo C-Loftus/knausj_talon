a19952d72325969b38e90897aa2e98cdca640c17
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-06-13 16:46:49 -0400

    document this better

--- pull.sh
+++ pull.sh
@@ -6,10 +6,10 @@ git merge community/main
 
 # Can you just have multiple branches in your fork to work from? One holding the stuff you want backed up, and one holding something synced with community for you to contribute from If that works for you then the command is git remote add upstream <url>. Then run git fetch upstream , create a new branch, switch to it, and run git reset --hard upstream/main . Then your branch is at base community. Switch upstream out for whatever you want to call community. 
 
+# git push -u origin bare_upstream
 # git checkout bare_upstream
 # git branch
 # git pull upstream main
-# git pull upstream
 # git reset --hard upstream/main
 # git checkout -b "bare_upstream"
 # git fetch upstream
